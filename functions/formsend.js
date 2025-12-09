ce17c645a05fdfda647714a232a9bc1f12424464
mkes99 <mike@formativewebsolutions.com>
2025-12-09 11:58:03 -0700

    adding email

---
+++ functions/formsend.js
@@ -0,0 +1,12 @@
+export async function onRequestPost(context) {
+    console.log("Received form submission:", context.request);
+     try {
+        await context.env.EMAIL_WORKER.sendEmail(context.request);
+
+        return new Response('Form submitted successfully!', { status: 200 });
+    } catch (error) {
+        console.error('Error processing form submission:', error);
+        return new Response('Error processing form submission', { status: 500 });
+    }
+
+}
---
+++ wp-includes/js/formSend.js
@@ -0,0 +1,50 @@
+// Basic form handling
+const form = document.querySelector("#email-form");
+let formReturnHtml = `
+    <div style="text-align: center; padding: 2rem;">
+    <i class="fas fa-check-circle" style="font-size: 3rem); margin-bottom: 1.5rem;"></i>
+    <h2 class="form-heading">Thank You!</h2>
+    <p style="font-size: 1.1rem; margin-bottom: 1rem;">Thank you for contacting me. I will get back with you as soon as possible.</p>
+    </div>
+`
+
+async function sendData() {
+    // Associate the FormData object with the form element
+    const formData = new FormData(form);
+    console.log("Form data.", formData);
+    const formValues = Object.fromEntries(formData.entries());
+    console.log("Form values.", formValues);
+
+    const response = await fetch("/formsend", {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formValues),
+    });
+
+    if (!response.ok) {
+        console.error('Error sending email:', JSON.stringify(error));
+        formReturnHtml = `
+            <div style="text-align: center; padding: 2rem;">
+            <i class="fas fa-exclamation-circle" style="font-size: 3rem; margin-bottom: 1.5rem;"></i>
+            <h2 class="form-heading">Oops!</h2>
+            <p style="font-size: 1.1rem; margin-bottom: 1rem;">There was a problem sending your message. Please try again later.</p>
+            </div>
+        `;
+    }
+
+    // Replace the form with a thank you message
+    const formCard = document.querySelector("#email-form");
+    formCard.innerHTML = formReturnHtml;
+
+    // Clear the form
+    form.reset();
+}
+
+
+// Take over form submission
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendData();
+});
